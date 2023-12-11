ad476d115de5cf5f87972a4240f1428c612ecf0f
TanamiUsagiri <99175410+TanamiUsagiri@users.noreply.github.com>
2023-12-11 14:17:40 +0700

    Update be-signup.php

--- be-signup.php
+++ be-signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LoginPage</title>
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="be-css.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
